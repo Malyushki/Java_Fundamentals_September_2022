014a40b30683a017a098a33baf6f5d9a50d848f6
Pavlin Malyushki <malyushki@gmail.com>
2022-10-10 05:39:49 +0300

    finished some Lists-MoreExercises Lists-Lab - done

--- Lists-Lab/src/MergingLists_03.java
+++ Lists-Lab/src/MergingLists_03.java
@@ -10,18 +10,13 @@ public class MergingLists_03 {
         List<Integer> numbers1 = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> numbers2 = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> combined = new ArrayList<>();
-        int count = 0;
+
         while (numbers1.size() > 0 && numbers2.size() > 0) {
-            if (count % 2 == 0) {
-                combined.add(numbers1.get(0));
-                combined.add(numbers2.get(0));
-            } else {
-                combined.add(numbers2.get(0));
-                combined.add(numbers1.get(0));
-            }
+            combined.add(numbers1.get(0));
+            combined.add(numbers2.get(0));
             numbers1.remove(0);
             numbers2.remove(0);
-            count++;
+
         }
         if (numbers1.size() > 1) {
             for (int i = 0; i < numbers1.size(); i++) {
@@ -29,12 +24,12 @@ public class MergingLists_03 {
 
             }
         }
-            if (numbers2.size() > 1) {
-                for (int i = 0; i < numbers1.size(); i++) {
-                    combined.add(numbers2.get(i));
+        if (numbers2.size() > 1) {
+            for (int i = 0; i < numbers2.size(); i++) {
+                combined.add(numbers2.get(i));
 
-                }
             }
+        }
         System.out.println(combined.toString());
     }
 }
---
+++ Lists-MoreExercise/src/TakeSkipRope_03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TakeSkipRope_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StringBuilder text = new StringBuilder(scanner.nextLine());
+        for (int i = 0; i <text.length() ; i++) {
+            if (Character.isDigit(text.charAt(i))){
+                
+            }
+
+        }
+    }
+}
