883a7f767a1d1a9227fafb4462aefe67b868bcb8
Pavlin Malyushki <malyushki@gmail.com>
2022-11-05 23:17:55 +0200

    MapsLambdaAndStreamAPI-Exercise

--- MapsLambdaAndStreamAPI-Exercise/src/ForceBook_09.java
+++ MapsLambdaAndStreamAPI-Exercise/src/ForceBook_09.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ForceBook_09 {
     public static void main(String[] args) {
@@ -10,11 +7,11 @@ public class ForceBook_09 {
         LinkedHashMap<String, List<String>> theForce = new LinkedHashMap<>();
         while (!command.equals("Lumpawaroo")) {
             if (command.contains(" | ")) {
-                String forceSide = command.split(" | ")[0];
-                String forceUser = command.split(" | ")[1];
+                String forceSide = command.split(" \\| ")[0];
+                String forceUser = command.split(" \\| ")[1];
                 theForce.putIfAbsent(forceSide, new ArrayList<>());
 
-                if (!theForce.containsValue(forceUser)) {
+                if (!isContained(theForce, forceUser)) {
                     theForce.get(forceSide).add(forceUser);
                 }
 
@@ -22,16 +19,42 @@ public class ForceBook_09 {
                 String forceSide = command.split(" -> ")[1];
                 String forceUser = command.split(" -> ")[0];
                 theForce.putIfAbsent(forceSide, new ArrayList<>());
-                if (!theForce.containsValue(forceUser)) {
+                if (!isContained(theForce, forceUser)) {
                     theForce.get(forceSide).add(forceUser);
 
-                }else {
-                    theForce.remove()
+
+                } else {
+                    theForce.values().removeIf(v->v.contains(forceUser));
+
+                    //map.values().removeIf(v -> v.equals(myValue));
+                    theForce.get(forceSide).add(forceUser);
+
                 }
+                System.out.printf("%s joins the %s side!%n", forceUser, forceSide);
             }
-
-
             command = scanner.nextLine();
         }
+
+        for (Map.Entry<String, List<String>> entry : theForce.entrySet()) {
+            if (!entry.getValue().isEmpty()) {
+                System.out.printf("Side: %s, Members: %d%n", entry.getKey(), entry.getValue().size());
+                for (String name : entry.getValue()) {
+                    System.out.printf("! %s%n", name);
+                }
+            }
+        }
+
+    }
+
+    static boolean isContained(LinkedHashMap<String, List<String>> map, String user) {
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            for (String name : entry.getValue()) {
+                if (name.equals(user)) {
+                    return true;
+
+                }
+            }
+        }
+        return false;
     }
 }
