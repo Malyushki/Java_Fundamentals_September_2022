dff36a5b73545133d48366872a4488779ac7cc44
malyushki <malyushki@gmail.com>
2022-11-11 14:09:11 +0200

    done

--- RegularExpressions-Exercises/src/Furniture_01.java
+++ RegularExpressions-Exercises/src/Furniture_01.java
@@ -1,41 +1,35 @@
-    import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Furniture_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+        String redex = ">>(?<furniture>[A-z]+)<<(?<price>[0-9.]+)!(?<quantity>[0-9]+)\\b";
+        Pattern pattern = Pattern.compile(redex);
+        double totalMoney = 0;
         List<String> items = new ArrayList<>();
-        List<Double> price = new ArrayList<>();
-        List<Integer> quantity = new ArrayList<>();
-        Double totalPrice = 0.0;
-        while (!command.equals("Purchase")) {
-            if (command.contains("<<")&&command.contains(">>")&&command.contains("!")) {
-                String[] order = command.replaceAll("[<<!]", " ").replaceAll("[>>]", "")
-                        .split("\\s+");
 
-                if (order.length == 3) {
-                    items.add(order[0]);
-                    price.add(Double.parseDouble(order[1]));
-                    quantity.add(Integer.parseInt(order[2]));
-                }
+        String command = scanner.nextLine();
+        while (!command.equals("Purchase")) {
+            Matcher orderMatcher = pattern.matcher(command);
+            while (orderMatcher.find()) {
+                items.add(orderMatcher.group("furniture"));
+                double price = Double.parseDouble(orderMatcher.group("price"));
+                double quantity = Double.parseDouble(orderMatcher.group("quantity"));
+                totalMoney = totalMoney + price * quantity;
             }
             command = scanner.nextLine();
         }
-
-
-        System.out.println("Bought furniture:");
-        if (items.size() > 0) {
-            for (int i = 0; i < items.size(); i++) {
-                totalPrice += price.get(i) * quantity.get(i);
+     //   if (items.size() > 0) {
+            System.out.println("Bought furniture:");
+            for (String item : items) {
+                System.out.println(item);
             }
-            for (int i = 0; i < items.size(); i++) {
-                System.out.println(items.get(i));
-            }
-        }
-        System.out.printf("Total money spend: %.2f", totalPrice);
+      //  }
+        System.out.printf("Total money spend: %.2f", totalMoney);
+
     }
-
 }
-
---
+++ RegularExpressions-Exercises/src/SoftUniBarIncome_03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SoftUniBarIncome_03 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+
+        while (!input.equals("end of shift")){
+
+
+
+            input=scanner.nextLine();
+        }
+
+    }
+}
