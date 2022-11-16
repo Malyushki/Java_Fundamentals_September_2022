752d6fddb17a23da54e33cbe7ff890daad06cb3c
Pavlin Malyushki <malyushki@gmail.com>
2022-11-16 08:38:36 +0200

    Exercise

--- RegularExpressions-MoreExercise/src/WinningTicket_01.java
+++ RegularExpressions-MoreExercise/src/WinningTicket_01.java
@@ -5,41 +5,50 @@ import java.util.regex.Pattern;
 public class WinningTicket_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().replaceAll("\\s+","").split(",");
-        String regex = "(?<leftSide>([@#$^]){6,10})[^ ]*(?<rightSide>\\1)";
-        boolean isMatch = false;
+        String[] input = scanner.nextLine().replaceAll("\\s+", "").split(",");
+        String regex = "[^@#$^]{0,10}(?<code>[@#$^]{6,10})[^@#$^]{0,10}";
         Pattern pattern = Pattern.compile(regex);
         for (int i = 0; i < input.length; i++) {
-            isMatch = false;
-            String currentTicket = input[i];
-            if (currentTicket.length()<20){
+            StringBuilder currentTicket = new StringBuilder(input[i]);
+            if (currentTicket.length()!=20){
                 System.out.println("invalid ticket");
-                isMatch = true;
             }else {
-                Matcher matcher = pattern.matcher(currentTicket);
-                while (matcher.find()) {
-                    String leftSide = matcher.group("leftSide");
-                    String rightSide = matcher.group("rightSide");
-                    int size = leftSide.length();
-                    char symbol = leftSide.charAt(0);
-                    if (size == 10) {
-                        System.out.printf("ticket \"%s\" - %d%s Jackpot!%n", currentTicket, size, symbol);
-                        isMatch = true;
-                        break;
-                    } else {
-                        System.out.printf("ticket \"%s\" - %d%s%n", currentTicket, size, symbol);
-                        isMatch = true;
-                        break;
-                    }
-                }
-                    if (!isMatch){
-                        System.out.printf("ticket \"%s\" - no match",currentTicket);
+                String leftSideTicket = currentTicket.substring(0,10);
+                String rightSideTicket = currentTicket.substring(10,20);
 
+              //  System.out.println(leftSideTicket);
+               // System.out.println(rightSideTicket);
+
+                Matcher leftMatcher = pattern.matcher(leftSideTicket);
+                Matcher rightMatcher = pattern.matcher(rightSideTicket);
+                while (leftMatcher.find()){
+                    String winningLeftCode = leftMatcher.group("code");
+                    while (rightMatcher.find()){
+                        String winningRightCode = rightMatcher.group("code");
+                        if (winningRightCode.equals(winningLeftCode)) {
+                            int countWiningSymbols = winningRightCode.length();
+                            if (countWiningSymbols==10){
+                                System.out.printf("ticket \"%s\" - 10%s Jackpot!%n",currentTicket,winningLeftCode.charAt(0));
+                            }else {
+
+                                System.out.printf("ticket \"%s\" - %d%s%n",currentTicket,countWiningSymbols,winningLeftCode.charAt(0));
+                            }
+
+
+                        }else {
+                            System.out.printf("ticket \"%s\" - no match",currentTicket);
+                        }
                     }
+
                 }
+
+
             }
 
         }
 
+
+
     }
+}
 
