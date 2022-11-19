f8dd952709e702e4ff6a0414a9d6714871e88b21
Pavlin Malyushki <malyushki@gmail.com>
2022-11-19 23:03:52 +0200

    ProgrammingFundamentalsFinalExam_02

--- EXAMS/ProgrammingFundamentalsFinalExam_02/src/WorldTour_01.java
+++ EXAMS/ProgrammingFundamentalsFinalExam_02/src/WorldTour_01.java
@@ -1,45 +1,47 @@
-import java.util.Arrays;
-import java.util.Scanner;
+    import java.util.Arrays;
+    import java.util.Scanner;
 
-public class WorldTour_01 {
-    public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        StringBuilder stops = new StringBuilder(scanner.nextLine());
-        String command = scanner.nextLine();
-        while (!command.equals("Travel")){
-           String[] commandType = command.split(":");
-            switch (commandType[0]){
-                case "Add Stop" :
-                    int indexAddStop = Integer.parseInt(commandType[1]);
-                    String stopAdd = commandType[2];
-                    if (indexAddStop>=0 && indexAddStop<stops.length()){
-                        stops.insert(indexAddStop,stopAdd);
-                        System.out.println(stops.toString());
-                    }
+    public class WorldTour_01 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String travelList = scanner.nextLine();
+            String command = scanner.nextLine();
 
-                    break;
-                case "Remove Stop" :
-                    int startIndex= Integer.parseInt(commandType[1]);
-                    int endIndex= Integer.parseInt(commandType[2]);
-                    if (stops.length()>0) {
-                        if (startIndex >= 0 && endIndex <= stops.length()) {
-                            endIndex++;
-                            stops.replace(startIndex, endIndex, "");
-                            System.out.println(stops.toString());
+            while (!command.equals("Travel")) {
+                String[] commandType = command.split(":");
+                switch (commandType[0]) {
+                    case "Add Stop":
+                        int indexForAdd = Integer.parseInt( commandType[1]);
+                        String addText = commandType[2];
+                       StringBuilder builder = new StringBuilder(travelList);
+                       builder.insert(indexForAdd,addText);
+                       travelList = builder.toString();
+                       System.out.println(travelList);
+                        break;
+                    case "Remove Stop":
+                        int startIndex = Integer.parseInt(commandType[1]);
+                        int endIndex = Integer.parseInt(commandType[2]);
+
+                        if (startIndex >= 0 && startIndex < travelList.length() && endIndex >= 0
+                                && endIndex < travelList.length()) {
+                        builder = new StringBuilder(travelList);
+                        builder.replace(startIndex,endIndex+1,"");
+                        travelList = builder.toString();
                         }
-                    }
-                    break;
-                case "Switch" :
-                    String oldString = commandType[1];
-                    String newString = commandType[2];
-                  // while (stops.toString().contains(oldString)){
-                       stops = new StringBuilder(stops.toString().replaceAll(oldString,newString));
-                       System.out.println(stops.toString());
-              //  }
-                    break;
+                        System.out.println(travelList);
+                        break;
+                    case "Switch":
+                        String oldString = commandType[1];
+                        String newString = commandType[2];
+                        if (travelList.contains(oldString)) {
+                            travelList = travelList.replaceAll(oldString, newString);
+                        }
+                        System.out.println(travelList);
+                        break;
+                }
+
+                command = scanner.nextLine();
             }
-            command = scanner.nextLine();
+            System.out.println("Ready for world tour! Planned stops: " + travelList.toString());
         }
-        System.out.println("Ready for world tour! Planned stops: " + stops.toString());
     }
-}
